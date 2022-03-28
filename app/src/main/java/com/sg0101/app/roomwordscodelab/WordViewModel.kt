2dcba3807ecad1a878a36c0086ef01745916266b
leesg0101 <leesg0101@gmail.com>
2022-03-28 20:35:55 +0900

    connect data

--- app/src/main/java/com/sg0101/app/roomwordscodelab/WordViewModel.kt
+++ app/src/main/java/com/sg0101/app/roomwordscodelab/WordViewModel.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.launch
 
 class WordViewModel(private val repository: WordRepository) : ViewModel() {
 
-    val allwords: LiveData<List<Word>> = repository.allWords.asLiveData()
+    val allWords: LiveData<List<Word>> = repository.allWords.asLiveData()
 
     fun insert(word: Word) = viewModelScope.launch {
         repository.insert(word)
