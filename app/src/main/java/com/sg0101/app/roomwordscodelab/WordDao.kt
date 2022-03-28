53089e0c94c672f6561f47c80ce94293b662d703
leesg0101 <leesg0101@gmail.com>
2022-03-28 19:55:24 +0900

    add viewmodel

--- app/src/main/java/com/sg0101/app/roomwordscodelab/WordDao.kt
+++ app/src/main/java/com/sg0101/app/roomwordscodelab/WordDao.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.flow.Flow
 interface WordDao {
 
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    fun getAlphabetizedWords(): Flow<Word>
+    fun getAlphabetizedWords(): Flow<List<Word>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(word: Word)
---
+++ app/src/main/java/com/sg0101/app/roomwordscodelab/WordRepository.kt
@@ -0,0 +1,16 @@
+package com.sg0101.app.roomwordscodelab
+
+import androidx.annotation.WorkerThread
+import kotlinx.coroutines.flow.Flow
+
+class WordRepository(private val wordDao: WordDao) {
+
+    val allWords: Flow<List<Word>> = wordDao.getAlphabetizedWords()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+
+}
