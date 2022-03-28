4f620351f704f1cdc62a026ca37a02099e873cd5
leesg0101 <leesg0101@gmail.com>
2022-03-28 20:03:18 +0900

    add xml layout

---
+++ app/src/main/java/com/sg0101/app/roomwordscodelab/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sg0101.app.roomwordscodelab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
