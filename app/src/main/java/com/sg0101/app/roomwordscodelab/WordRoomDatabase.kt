938e9dd6fca77ef95fe55806e40516d173f62c96
leesg0101 <leesg0101@gmail.com>
2022-03-28 19:44:06 +0900

    add room database

---
+++ app/src/main/java/com/sg0101/app/roomwordscodelab/WordRoomDatabase.kt
@@ -0,0 +1,29 @@
+package com.sg0101.app.roomwordscodelab
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Word::class], version = 1)
+abstract class WordRoomDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_databases"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
